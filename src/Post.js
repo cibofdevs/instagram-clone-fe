1cdd5c8e0b0ce1bd3b07492e633a2672d75dfddd
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-12 09:06:51 +0700

    add caption and comments

--- src/Post.js
+++ src/Post.js
@@ -5,19 +5,33 @@ const BASE_URL = "http://localhost:8000/"
 
 function Post({ post }) {
 
-    const [imageUrl, setImageUrl] = useState("")
+    const [imageUrl, setImageUrl] = useState("");
+    const [comments, setComments] = useState([])
+
 
     useEffect(() => {
         if (post.image_url_type === "absolute") {
-            setImageUrl(post.image_url)
+            setImageUrl(post.image_url);
         } else {
-            setImageUrl(BASE_URL + post.image_url)
+            setImageUrl(BASE_URL + post.image_url);
         }
+    }, []);
+
+    useEffect(() => {
+        setComments(post.comments)
     }, []);
 
     return (
         <div className="post">
             <img className="post-image" src={imageUrl} />
+            <h4 className="post-text">{post.caption}</h4>
+            <div className="post-comments">
+                {
+                    comments.map((comment) => (
+                        <p><strong>{comment.username}:</strong> {comment.text}</p>
+                    ))
+                }
+            </div>
         </div>
     )
 }
