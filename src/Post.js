2f2b757788472adcd518eafd6be5adc6388cfaef
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-13 17:54:47 +0700

    retrieve updated comments

--- src/Post.js
+++ src/Post.js
@@ -70,11 +70,30 @@ function Post({ post, authToken, authTokenType, username }) {
                 }
                 throw response;
             })
+            .then(data => {
+                fetchComments();
+            })
             .catch(error => {
                 console.log(error);
             })
             .finally(() => {
                 setNewComment("");
+            })
+    }
+
+    const fetchComments = () => {
+        fetch(BASE_URL + "comment/all/" + post.id)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw response;
+            })
+            .then(data => {
+                setComments(data);
+            })
+            .catch(error => {
+                console.log(error);
             })
     }
 
